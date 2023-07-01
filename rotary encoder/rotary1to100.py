97b483f5204f7ce479807efddd73356e761b28c0
Ryan <ryan4bondy+GitHub@gmail.com>
2023-07-01 16:28:28 -0700

    test

--- rotary encoder/rotary1to100.py
+++ rotary encoder/rotary1to100.py
@@ -11,6 +11,9 @@ GPIO.setup(SW_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 def my_callback(scale_position):
     print('Hello world! The scale position is {}'.format(scale_position))
 
+def switch_callback():
+    print('switch clicked')
+
 # Init the encoder pins
 my_encoder = pyky040.Encoder(CLK=22, DT=4, SW=6)
 # Or the encoder as a device (must be installed on the system beforehand!)
@@ -18,7 +21,7 @@ my_encoder = pyky040.Encoder(CLK=22, DT=4, SW=6)
 
 # Setup the options and callbacks (see documentation)
 my_encoder.setup(scale_min=0, scale_max=100, step=1, chg_callback=my_callback)
-
+my_encoder.setup(sw_callback=switch_callback)
 # Launch the listener
 my_encoder.watch()
 
