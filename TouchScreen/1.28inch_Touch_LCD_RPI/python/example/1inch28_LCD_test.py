ff7de8e1cc3893c215feb30190cb20f1b9f0e5eb
Ryan <ryan4bondy+GitHub@gmail.com>
2023-07-02 20:45:15 -0700

    Update 1inch28_LCD_test.py

--- TouchScreen/1.28inch_Touch_LCD_RPI/python/example/1inch28_LCD_test.py
+++ TouchScreen/1.28inch_Touch_LCD_RPI/python/example/1inch28_LCD_test.py
@@ -33,54 +33,55 @@ def Int_Callback(TP_INT):
 
 
 try:
-    ''' Warning!!!Don't  create multiple displayer objects!!! '''
-    disp = LCD_1inch28.LCD_1inch28()
-    # Initialize library.
-    disp.Init()
-    # Clear display.
-    disp.clear()
-    
-    touch.init()
-    
-    touch.int_irq(TP_INT,Int_Callback) 
-    # Create blank image for drawing.
-    # Common HTML color names.
-    image1 = Image.new("RGB", (disp.width, disp.height), "WHITE")
-    draw = ImageDraw.Draw(image1)
+    while True:
+        ''' Warning!!!Don't  create multiple displayer objects!!! '''
+        disp = LCD_1inch28.LCD_1inch28()
+        # Initialize library.
+        disp.Init()
+        # Clear display.
+        disp.clear()
+        
+        touch.init()
+        
+        touch.int_irq(TP_INT,Int_Callback) 
+        # Create blank image for drawing.
+        # Common HTML color names.
+        image1 = Image.new("RGB", (disp.width, disp.height), "WHITE")
+        draw = ImageDraw.Draw(image1)
 
-    #Gestures
-    Mode = 0
-    touch.Set_Mode(Mode)
-    Font1 = ImageFont.truetype("../Font/Font02.ttf",24)
-    Font = ImageFont.truetype("../Font/FlyingBirdFont.ttf",24)
-    draw.text((65, 80), 'Drink Maker', fill = "BLACK",font=Font)
-    draw.text((40, 120), 'By Seneca', fill = "BLACK",font=Font)
-    disp.ShowImage(image1)
-    time.sleep(5.5)
-    Font = ImageFont.truetype("../Font/FlyingBirdFont.ttf",35)    
-    draw.rectangle((0,0,240,240),fill = "WHITE", outline=None, width=1)
-    while touch.Gestures != 0x03:
-        draw.text((65, 90), 'LEFT', fill = "BLACK",font=Font)
+        #Gestures
+        Mode = 0
+        touch.Set_Mode(Mode)
+        Font1 = ImageFont.truetype("../Font/Font02.ttf",24)
+        Font = ImageFont.truetype("../Font/FlyingBirdFont.ttf",24)
+        draw.text((65, 80), 'Drink Maker', fill = "BLACK",font=Font)
+        draw.text((40, 120), 'By Seneca', fill = "BLACK",font=Font)
         disp.ShowImage(image1)
-        time.sleep(0.001)
+        time.sleep(5.5)
+        Font = ImageFont.truetype("../Font/FlyingBirdFont.ttf",35)    
+        draw.rectangle((0,0,240,240),fill = "WHITE", outline=None, width=1)
+        while touch.Gestures != 0x03:
+            draw.text((65, 90), 'LEFT', fill = "BLACK",font=Font)
+            disp.ShowImage(image1)
+            time.sleep(0.001)
 
-    draw.rectangle((0,0,240,240),fill = "WHITE", outline=None, width=1)
-    while touch.Gestures != 0x04:
-        draw.text((55, 90), 'RIGHT', fill = "BLACK",font=Font)
-        disp.ShowImage(image1)
-        time.sleep(0.001)
+        draw.rectangle((0,0,240,240),fill = "WHITE", outline=None, width=1)
+        while touch.Gestures != 0x04:
+            draw.text((55, 90), 'RIGHT', fill = "BLACK",font=Font)
+            disp.ShowImage(image1)
+            time.sleep(0.001)
 
-    draw.rectangle((0,0,240,240),fill = "WHITE", outline=None, width=1)
-    while touch.Gestures != 0x0C:
-        draw.text((35, 90), 'Long Press', fill = "BLACK",font=Font)
-        disp.ShowImage(image1)
-        time.sleep(0.001)
+        draw.rectangle((0,0,240,240),fill = "WHITE", outline=None, width=1)
+        while touch.Gestures != 0x0C:
+            draw.text((35, 90), 'Long Press', fill = "BLACK",font=Font)
+            disp.ShowImage(image1)
+            time.sleep(0.001)
 
-    draw.rectangle((0,0,240,240),fill = "WHITE", outline=None, width=1)
-    while touch.Gestures != 0x0B:
-        draw.text((30, 90), 'Double Click', fill = "BLACK",font=Font)
-        disp.ShowImage(image1)
-        time.sleep(0.001)
+        draw.rectangle((0,0,240,240),fill = "WHITE", outline=None, width=1)
+        while touch.Gestures != 0x0B:
+            draw.text((30, 90), 'Double Click', fill = "BLACK",font=Font)
+            disp.ShowImage(image1)
+            time.sleep(0.001)
 
 except IOError as e:
     logging.info(e)    
