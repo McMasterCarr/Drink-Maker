a096b4099e949a8960e0d8a250f23647d7bdb9f6
Ryan <ryan4bondy+GitHub@gmail.com>
2023-07-03 23:21:14 -0700

    Update test.py

--- TouchScreen/1.28inch_Touch_LCD_RPI/python/example/test.py
+++ TouchScreen/1.28inch_Touch_LCD_RPI/python/example/test.py
@@ -65,9 +65,9 @@ def sizeOptions():
             drinkSize+=1
             if drinkSize > 3 :
                 drinkSize = 3
-        if touch.Gestures == 0x0C:
-            dispensing = True
+        if touch.Gestures == 0x05:
             dispense()
+            dispensing = True
         draw.rectangle((0,0,240,240),fill = "BLACK", outline=None, width=1)
         draw.text((65, 90), str(drinkSize), fill = "WHITE",font=Font)
         disp.ShowImage(image2)
